116cbe46426f7b13086db056916551fa6809e28e
inikerjayaa <iniemailkerjayaa@gmail.com>
2026-09-17 18:10:00 +0700

    V37 part 2A: wire native splash after branding

--- bootstrap.sh
+++ bootstrap.sh
@@ -31,6 +31,7 @@ cp -a "$TMP_ROOT/arus_native_shell/ios" ./ios
 
 python3 tool/native_hardening.py
 python3 tool/native_branding.py
+python3 tool/native_splash.py
 bash tool/run_all_audits.sh
 if [[ -f pubspec.lock ]]; then
   flutter pub get --enforce-lockfile
